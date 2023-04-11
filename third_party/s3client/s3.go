c9214769e68c7f71b9e0bf5813af1f86782a0931
cachalots <wszwcy@gmail.com>
2023-04-11 15:14:11 +0800

    transform blockstore key

--- third_party/s3client/s3.go
+++ third_party/s3client/s3.go
@@ -9,6 +9,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"io"
+	"path/filepath"
+)
+
+const (
+	keyBlockSize    = 3
+	keyMaxSliceSize = 3
 )
 
 type S3Storage struct {
@@ -39,8 +45,6 @@ func NewS3Client(storage S3Config) *S3Storage {
 	}
 
 	srv := s3.New(sess, awsConfig)
-	//client, err := oss.New(storage.GetEndpoint(), storage.GetAccessKey(), storage.GetSecretKey())
-
 	bucket := storage.GetBucket()
 	_, err := srv.HeadBucket(&s3.HeadBucketInput{Bucket: &bucket})
 	if err != nil {
@@ -67,7 +71,7 @@ func NewS3Client(storage S3Config) *S3Storage {
 func (storage *S3Storage) Get(ctx context.Context, key string) (io.ReadCloser, error) {
 	output, err := storage.srv.GetObjectWithContext(ctx, &s3.GetObjectInput{
 		Bucket: &storage.bucket,
-		Key:    &key,
+		Key:    storage.transformKey(key),
 	})
 	if err != nil {
 		return nil, err
@@ -78,7 +82,7 @@ func (storage *S3Storage) Get(ctx context.Context, key string) (io.ReadCloser, e
 func (storage *S3Storage) Put(ctx context.Context, key string, hash string, data []byte) error {
 	contentLength := int64(len(data))
 	_, err := storage.srv.PutObjectWithContext(ctx, &s3.PutObjectInput{
-		Key:    &key,
+		Key:    storage.transformKey(key),
 		Body:   bytes.NewReader(data),
 		Bucket: &storage.bucket,
 
@@ -93,7 +97,7 @@ func (storage *S3Storage) Put(ctx context.Context, key string, hash string, data
 func (storage *S3Storage) Delete(ctx context.Context, key string) error {
 	_, err := storage.srv.DeleteObjectWithContext(ctx, &s3.DeleteObjectInput{
 		Bucket: &storage.bucket,
-		Key:    &key,
+		Key:    storage.transformKey(key),
 	})
 	return err
 }
@@ -101,7 +105,7 @@ func (storage *S3Storage) Delete(ctx context.Context, key string) error {
 func (storage *S3Storage) GetSize(ctx context.Context, key string) (int, error) {
 	output, err := storage.srv.HeadObjectWithContext(ctx, &s3.HeadObjectInput{
 		Bucket: &storage.bucket,
-		Key:    &key,
+		Key:    storage.transformKey(key),
 	})
 	if err != nil {
 		return 0, err
@@ -110,4 +114,19 @@ func (storage *S3Storage) GetSize(ctx context.Context, key string) (int, error) 
 		return 0, errors.New("parse content length failed")
 	}
 	return int(*output.ContentLength), nil
+}
+
+func (storage *S3Storage) transformKey(key string) *string {
+	size := keyMaxSliceSize
+	if c := len(key) / keyBlockSize; c <= keyMaxSliceSize {
+		size = c
+	}
+	suffix := key[len(key)-size*keyBlockSize:]
+	pathSlice := make([]string, size+1)
+	pathSlice[size] = key
+	for i := 0; i < size; i++ {
+		from, to := i*keyBlockSize, (i*keyBlockSize)+keyBlockSize
+		pathSlice[i] = suffix[from:to]
+	}
+	return aws.String(filepath.Join(pathSlice...))
 }
