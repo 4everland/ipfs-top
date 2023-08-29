e02744c14668950a9678cb68b0a5e5100b27c8a6
cachalots <wszwcy@gmail.com>
2023-08-29 17:11:44 +0800

    ignore gorm record not found error

--- app/blockstore/internal/biz/biz.go
+++ app/blockstore/internal/biz/biz.go
@@ -5,6 +5,10 @@ import (
 	"github.com/google/wire"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 // ProviderSet is data providers.
@@ -15,7 +19,13 @@ func NewIndexStore(data *conf.Data) (BlockIndex, error) {
 	case conf.Data_TiKV:
 		return NewTiKv(data.GetDb().GetTikv().GetAddrs()...)
 	case conf.Data_PG:
-		d, err := gorm.Open(postgres.Open(data.GetDb().GetPg().GetDsn()))
+		d, err := gorm.Open(postgres.Open(data.GetDb().GetPg().GetDsn()), &gorm.Config{
+			Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+				SlowThreshold:             200 * time.Millisecond,
+				LogLevel:                  logger.Warn,
+				IgnoreRecordNotFoundError: true,
+			}),
+		})
 		if err != nil {
 			return nil, err
 		}
